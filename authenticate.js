6db8991e005a28dac05eb1643a5cb7dc799f5a71
Logan Hall <info.tech.logan@gmail.com>
2023-09-29 17:32:52 -0400

    Passport JWT

--- authenticate.js
+++ authenticate.js
@@ -2,6 +2,40 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+const config = require('./config.js');
+
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
+};
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // this is a function that accepts a request as an argument and returns either the JWT as a string or null
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(
+    new JwtStrategy(
+        opts,
+        (jwt_payload, done) => {
+            console.log('JWT payload:', jwt_payload);
+            User.findOne({ _id: jwt_payload._id }, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                } else if (user) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            });
+        }
+    )
+);
+
+exports.verifyUser = passport.authenticate('jwt', { session: false });
